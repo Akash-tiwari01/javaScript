75cac7a1dba9481ba48a73eefc55aa0e0993258c
Tiwariji01 <akashtiwari19bca014@gmail.com>
2025-01-09 21:49:08 +0530

    javaScript function

---
+++ function.js
@@ -0,0 +1,118 @@
+// // // // functions in javaScript 
+
+// // // // console.log("Hello world"); // print hello world
+
+// // // // console.log("Hello world"); // print hello world
+
+// // // // console.log("Hello world"); // print hello world
+
+// // // // console.log("Hello world"); // print hello world
+
+// // // // console.log("Hello world"); // print hello world
+
+
+// // // // function printHello(); // declare 
+// // // function printHello(){
+
+// // //     console.log("Hello world"); // print hello world 
+
+// // // } 
+
+
+
+// // // // printHello 
+
+// // // printHello()  // function call
+
+
+// // // printHello()
+
+
+// // // printHello()
+
+
+// // // // // function parameter or argument
+
+// // // function add(num1 , num2){  // parameterS
+// // //     console.log(num1 + num2);
+// // // }
+
+
+// // // // function call
+
+// // // add(5,2);   // Arguments
+
+
+// // // add(38,32);
+
+
+// // // return
+
+// // function add(num1 , num2){
+
+// //     console.log("this is addition function");  
+// //     return  num1 + num2;
+    
+// // }
+
+
+// // console.log("value on function call  :"+add(20, 30))
+
+
+
+// // function greet(name){
+// //     console.log(`Hello ${name}, Welcome to our app`)
+// // }
+
+// // greet("Anmol");
+
+
+// //greet// function name kya hold krta hn ?
+// // function ka address 
+
+// // jaha bhi function ka address use hoga vaha hum log function ka name denge without () "greet"
+// //jaha bhi function ko execute karana hoga vaha function name () "greet()" use karenge 
+
+// // console.log(`function name has: ${greet}`);
+
+// function greet(name){
+//     console.log(`Hello ${name}, Welcome to our app`)
+// }
+// const sayHello = greet
+
+
+// sayHello("Shreya");
+
+
+// console.log(sayHello());
+// function add(num1, num2){ return num1 + num2; }
+
+
+//function (num1, num2){ return num1 + num2; }  // anonymous function
+
+
+//let fun = function (num1, num2){ return num1 + num2; }  // anonymous function 
+
+// let a = fun(3,5);
+// let b = add(3,5);
+
+//let fun = (num1, num2)=>{ return num1 + num2; } // arrow function
+
+// (parameters) => {} syntax of aerrow function 
+
+let fun = (num1, num2)=> (num1 + num2 )  // one liner function 
+
+// (parameter) => (expression)  
+// jabbhi hum log {} ka use kr rahe hn arrow function me to hum logo ko return keyword ka use krna hoga 
+// jabbhi hum log () ka use kr rahe hn tb vo apne aap value return karega   
+
+console.log(`value in fun(): ${fun(3,5)}`);
+// console.log(`value in add(): ${add(3,5)}`);
+
+// github 
+// function 
+// parameters / arguments
+// return keyword 
+// ananomous function 
+// arrow function 
+// oneliner function 
