22006eacb5d2b4c70a45d53cea3c677061c5f506
Tiwariji01 <akashtiwari19bca014@gmail.com>
2025-01-12 18:14:51 +0530

    objects

---
+++ object.js
@@ -0,0 +1,50 @@
+// let student = {
+//     name:"Anmol",
+//     course:"web dev",
+//     email:"Anmol@1234gmail.com",
+//     phoneNO:23456789012,
+//     isActive:false
+// }
+
+// using Constructor 
+// using new keyword
+// using literals
+
+let b = [2,4,7,9] //Arrays index: 0,1,2,3;  elements => values
+
+
+let student = {
+    // key: value,
+    name:"Ritik",
+    email:"ritik123@gmail.com",
+    phoneNo:1234567890,
+    subjects:["html","css","js"],
+    address:{
+        buldingNo:"34",
+        Area:"karolbagh",
+        city:"New Delhi",
+        pincode:111000
+    },
+    isLogin:true,
+    "is a prime member":WebTransportDatagramDuplexStream
+} //object ;
+// oject creation using new keyword
+
+let employee = new Object({name:"Akash",email:"123@gmail.com"})// {}
+
+
+
+// how to access values 
+// object_name.value
+student["is a prime member"]
+
+// console.log(student['address']);
+
+console.log(object.key(student));
+
+// console.log(employee);
+
+
+
+
+
